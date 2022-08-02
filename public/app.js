7868a1890a5ed7ee9f16602f6452975599c1a953
LeugAp <71951797+LeugAp@users.noreply.github.com>
2022-08-02 11:44:46 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,36 +1,31 @@
-const img = document.getElementById('img');
-const title = document.getElementById('name');
-var time = 3000;
+// const img = document.getElementById('img');
+// const title = document.getElementById('name');
+// var time = 3000;
 
-const data = [
-    {
-        src: "flipper-color.jpg",
-        title: "Color Flipper"
-    },
-    {
-        src: "menu-food.jpg",
-        title: "Menu"
-    },
-    {
-        src: "tabs.jpg",
-        title: "Tabs"
-    }
-]
+// const data = [
+//     {
+//         src: "flipper-color.jpg",
+//         title: "Color Flipper"
+//     },
+//     {
+//         src: "menu-food.jpg",
+//         title: "Menu"
+//     },
+//     {
+//         src: "tabs.jpg",
+//         title: "Tabs"
+//     }
+// ]
 
-let aux = 0;
+// let aux = 0;
 
-function changeImg(){
-    if(aux == 3) aux = 0;
-    img.setAttribute("src", data[aux].src);
-    aux++;
-    setTimeout("changeImg()", time);
-}
+// function changeImg(){
+//     if(aux == 3) aux = 0;
+//     img.setAttribute("src", data[aux].src);
+//     aux++;
+//     setTimeout("changeImg()", time);
+// }
 
-window.onload = changeImg;
+// window.onload = changeImg;
 
 
-/*
-
-    Miguel Angel Apolo Cabrera
-    
-*/
